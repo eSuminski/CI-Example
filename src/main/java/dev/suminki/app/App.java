f261543ef1d936d0c97293adc136dd10e5c645ed
eSuminski <rctolinski@gmail.com>
2022-01-07 16:22:20 -0500

    made a few adjustments

--- src/main/java/dev/suminki/app/App.java
+++ src/main/java/dev/suminki/app/App.java
@@ -6,14 +6,11 @@ import dev.suminki.dal.GreetingDAOImp;
 import dev.suminki.sl.GreetingService;
 import dev.suminki.sl.GreetingServiceImp;
 import io.javalin.Javalin;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class App {
-    public static Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        logger.info("Creating Javalin");
         Javalin app = Javalin.create(
                 javalinConfig -> {
                     javalinConfig.enableCorsForAllOrigins();
@@ -21,7 +18,6 @@ public class App {
                 }
         );
 
-        logger.info("Creating DAO, Service, and Controller objects");
         GreetingDAO greetingDAO = new GreetingDAOImp();
         GreetingService greetingService = new GreetingServiceImp(greetingDAO);
         GreetingController greetingController = new GreetingController(greetingService);
@@ -30,7 +26,6 @@ public class App {
 
         app.get("/greeting/{id}", greetingController.getGreeting);
 
-        logger.info("Starting Javalin");
         app.start(8081);
 
     }
--- web-pages/greetingMethods.js
+++ web-pages/greetingMethods.js
@@ -1,7 +1,7 @@
 const greeting = document.getElementById("greeting");
 
 async function getGreetingMessage(id){
-    let response = await fetch (`http://localhost:8081/greeting/${id}`);
+    let response = await fetch (`http://3.86.88.161:8081/greeting/${id}`);
     if (response.status === 200){
         let message = await response.json();
         greeting.textContent = "";
