c7f5353b6034e89d8cf64135378b999c70c9cf82
eSuminski <rctolinski@gmail.com>
2022-01-07 17:02:22 -0500

    changed port for javalin

--- src/main/java/dev/suminki/app/App.java
+++ src/main/java/dev/suminki/app/App.java
@@ -6,11 +6,16 @@ import dev.suminki.dal.GreetingDAOImp;
 import dev.suminki.sl.GreetingService;
 import dev.suminki.sl.GreetingServiceImp;
 import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class App {
 
+    public static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+        logger.info("Creating Javalin object");
         Javalin app = Javalin.create(
                 javalinConfig -> {
                     javalinConfig.enableCorsForAllOrigins();
@@ -18,6 +23,7 @@ public class App {
                 }
         );
 
+        logger.info("Creating DAO, Service, and Controller objects");
         GreetingDAO greetingDAO = new GreetingDAOImp();
         GreetingService greetingService = new GreetingServiceImp(greetingDAO);
         GreetingController greetingController = new GreetingController(greetingService);
@@ -26,7 +32,8 @@ public class App {
 
         app.get("/greeting/{id}", greetingController.getGreeting);
 
-        app.start(8081);
+        logger.info("Starting Javalin on port ");
+        app.start(7000);
 
     }
 }
